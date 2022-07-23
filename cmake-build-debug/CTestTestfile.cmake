3bb6785c2a8b01f6bd424818808f6411281f0917
陶媛 <taoyuan@n181-199-002.byted.org>
2022-07-23 14:31:25 +0800

    second commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/taoyuan/CLionProjects/bushub
+# Build directory: /Users/taoyuan/CLionProjects/bushub/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest-build")
+subdirs("src")
+subdirs("test")
