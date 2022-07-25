bd446898815983dc405a77ece75a3213a7390d4f
陶媛 <taoyuan.saria@bytedance.com>
2022-07-25 16:03:36 +0800

    t

--- src/execution/distinct_executor.cpp
+++ src/execution/distinct_executor.cpp
@@ -9,6 +9,7 @@
 // Copyright (c) 2015-2021, Carnegie Mellon University Database Group
 //
 //===----------------------------------------------------------------------===//
+#include <unordered_map>
 
 #include "execution/executors/distinct_executor.h"
 
@@ -16,10 +17,38 @@ namespace bustub {
 
 DistinctExecutor::DistinctExecutor(ExecutorContext *exec_ctx, const DistinctPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx) {
+    plan_ = plan;
+    child_executor_ = std::move(child_executor);
+}
 
-void DistinctExecutor::Init() {}
+void DistinctExecutor::Init() {
+    child_executor_->Init();
 
-auto DistinctExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+    Tuple tup;
+    RID rid;
+    while (child_executor_->Next(&tup, &rid)) {
+        DistinctKey key;
+        int len = GetOutputSchema()->GetColumnCount();
+        for (int i = 0; i < len; i++) {
+            key.distinct_val_.emplace_back(tup.GetValue(GetOutputSchema(), i));
+        }
+        auto t = key;
+        // std::cout << tup. << std::endl;
+        if (map_.count(key) == 0) {
+            map_.insert({key, true});
+        }
+    }
+    std::cout << map_.size() << std::endl;
+    iter_ = map_.begin();
+}
+
+auto DistinctExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    if (iter_ == map_.end()) { return false; }
+
+    *tuple = Tuple(iter_->first.distinct_val_, GetOutputSchema());
+    iter_++;
+    return true;
+}
 
 }  // namespace bustub
